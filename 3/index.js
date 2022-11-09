2e15ed8be0836c824ff16422a2b424a414b68d19
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2022-11-09 17:08:51 +0000

    working module

--- 3/index.js
+++ 3/index.js
@@ -1,10 +1,3 @@
-/*
-Algorithm:
-
-Function valueOfNumeral(str) returns value of numeral. If str.length > 1, check for expcetion, return value. If str.length === 1, return value.
-
-*/
-
 let rulesArr = [
   { numeral: "I", value: 1 },
   { numeral: "V", value: 5 },
@@ -27,6 +20,21 @@ let s = "V";
 let s2 = "LVIII";
 let s3 = "MCMXCIV";
 
+function appLogic(str) {
+  sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    let currentAndNextChar = str[i] + str[i + 1];
+    let currentChar = str[i];
+    if (valueOfNumeral(currentAndNextChar).exception) {
+      sum += valueOfNumeral(currentAndNextChar).value;
+      i += 1;
+    } else {
+      sum += valueOfNumeral(currentChar).value;
+    }
+  }
+  return sum;
+}
+
 function valueOfNumeral(str) {
   let exception = null;
   if (str.length === 1) {
@@ -43,14 +51,9 @@ function valueOfNumeral(str) {
       if (str === exceptionsArray[i].numeral) {
         exception = true;
         return { value: exceptionsArray[i].value, exception: exception };
-      } else {
-        for (let i = 0; i < rulesArr.length; i++) {
-          if (str === rulesArr[i].numeral) {
-            exception = false;
-            return { value: rulesArr[i].value, exception: exception };
-          }
-        }
       }
     }
+    exception = false;
+    return { exception: exception };
   }
 }
