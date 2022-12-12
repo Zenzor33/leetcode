64fcfd7a85ca82e359c15e301b3469f7de48f1bc
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2022-12-12 19:20:21 -0300

    wtf

--- 6/index.js
+++ 6/index.js
@@ -1,16 +1,14 @@
-const list1 = [1, 2, 4];
-const list2 = [1, 3, 4];
+const list1 = [1, 2];
+const list2 = [1, 3];
 
 const edge1 = [1, 2, 3, 4];
 const edge2 = [1, 4, 5, 6];
 
 let newArr = [];
 var mergeTwoLists = function (list1, list2) {
-  let x = newArr;
-  console.log(x);
   let l1head = list1[0];
   let l2head = list2[0];
-  if (list1.length === 0 || list2.length === 0) return x;
+  if (list1.length === 0 || list2.length === 0) return newArr;
   if (l1head === l2head) {
     newArr.push(list1.shift());
     mergeTwoLists(list1, list2);
@@ -25,4 +23,5 @@ var mergeTwoLists = function (list1, list2) {
   }
 };
 
-console.log(mergeTwoLists(list1, list2));
+let ans = mergeTwoLists(list1, list2);
+console.log(ans);
